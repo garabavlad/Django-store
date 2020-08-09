c30ac4e44da85d7baae3db01b180eee170e23427
Vladislav Garaba <garaba.vlad@Vladislavs-MacBook-Pro.local>
2020-08-09 18:54:57 +0300

    initial

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.all()
+
+    return render(request, "index.html", {'products' : products})
+
+
+def new_product(request):
+    return HttpResponse('<b>New world</b>')
